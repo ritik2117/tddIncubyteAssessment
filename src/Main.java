2f5623c9f356ad307902c008f0f81ae2f994cde1
ritik2117 <ritikgupta21.17@gmail.com>
2024-02-17 13:45:57 +0530

    Added tst cases

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
+        /**
+         * Unit Test Cases:
+         * 1. Input: "", "1", "1,5"
+         * Output: 0, 1, 6
+         *
+         * 2. Input: 1\n2,3
+         * Output: 6
+         *
+         * 3. Input: //;\n1;2
+         * Output: 3
+         */
         System.out.print("Enter numbers\n");
         String number = scanner.nextLine();
         sb.append(number);
