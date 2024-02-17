3170283f4dc67cdf3e6af995bd20fd4722e4f974
Ritik Gupta <ritikgupta21.17@gmail.com>
2024-02-17 11:37:18 +0530

    Code to add comma sepearted numbers

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,3 +1,15 @@
 public class StringCalculator {
-
+    public static int add(String numbers) {
+//        Check if the input string is empty or null
+        if (numbers == null || numbers.isEmpty()) {
+            return 0;
+        }
+//        Split the input string by commas and sum up the integers
+        int sum = 0;
+        String[] numberArray = numbers.split(",");
+        for (String number : numberArray) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
+    }
 }
