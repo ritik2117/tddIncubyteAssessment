991f0b9b0cb261d0f2ae6b4b2b38ab0fe173f696
Ritik Gupta <ritikgupta21.17@gmail.com>
2024-02-17 12:03:53 +0530

    Code to get input in main method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        System.out.print("Enter a number (Type X to stop)\n");
+        String number = scanner.nextLine();
+        sb.append(number);
+        String input = sb.toString();
+        System.out.println("Input: " + input);
+        System.out.println("Output: " + StringCalculator.add(number));
+        return;
     }
 }
